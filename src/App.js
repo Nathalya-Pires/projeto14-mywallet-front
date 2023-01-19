4abaabdf9f711daca56da11aea49812fb54afb05
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-01-19 07:45:39 -0400

    layout tela home ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Cadastro from "./pages/Cadastro";
 import HomePage from "./pages/HomePage";
-
+import Home from "./pages/Home";
+import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
-    // <HomePage/>
-    // <Cadastro/>
-  )
+    <>
+      <GlobalStyle />
+      {/* <HomePage/>
+    <Cadastro/> */}
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import vetor from "../assets/Vector.png";
+import { AddCircleOutline, RemoveCircleOutline } from "react-ionicons";
+
+export default function Home() {
+  return (
+    <Container>
+      <Topo>
+        <p>Olá, Nathy</p>
+        <Sair src={vetor} alt={"Icone de sair"} />
+      </Topo>
+      <Registros>
+        <p>
+          Não há registros de <br></br>entrada ou saída
+        </p>
+      </Registros>
+      <ContainerRegistros>
+        <div>
+          <AddCircleOutline color={"#ffffff"} height="22px" width="22px" />
+          <p>Nova entrada</p>
+        </div>
+        <div>
+          <RemoveCircleOutline color={"#ffffff"} height="22px" width="22px" />
+          <p>Nova saída</p>
+        </div>
+      </ContainerRegistros>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #8c11be;
+  width: 375px;
+  height: 100%;
+  min-height: 667px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Topo = styled.div`
+  width: 85%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 30px;
+
+  p {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31px;
+    color: #ffffff;
+  }
+`;
+const Sair = styled.img`
+  width: 23px;
+  height: 24px;
+`;
+
+const Registros = styled.div`
+  width: 326px;
+  height: 446px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 25px;
+
+  p {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    color: #868686;
+  }
+`;
+const ContainerRegistros = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 13px 10px;
+
+  div {
+    width: 155px;
+    height: 114px;
+    background: #a328d6;
+    border-radius: 5px;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    color: #ffffff;
+    padding: 11px 9px;
+    margin: 10px;
+    p {
+      padding-top: 33px;
+    }
+  }
+`;
